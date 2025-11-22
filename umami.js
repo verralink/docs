4fbea1d47ea034baa1eb24b5f25b798217241705
verralink <rami@verralink.app>
2025-11-22 04:12:54 -0500

    Documentation edits made through Mintlify web editor

--- umami.js
+++ umami.js
@@ -1,7 +1,7 @@
 (function () {
-  if (!document.querySelector('script[src="https://analytics.verralink.app/script.js"]')) {
+  if (!document.querySelector('script[src="https://telemetry.verralink.app/script.js"]')) {
     const script = document.createElement("script");
-    script.src = "https://analytics.verralink.app/script.js";
+    script.src = "https://telemetry.verralink.app/script.js";
     script.async = true;
     script.dataset.websiteId = "caac8aa7-7b9d-47ee-9443-e3c636952ff8";
 
